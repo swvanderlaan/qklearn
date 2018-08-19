9aa1d939b61592fcce3a99822cf606faf239d479
tbezemer <bezemer.tim@gmail.com>
2018-08-19 12:52:10 +0200

    Small bug fix

--- qklearn/tools.py
+++ qklearn/tools.py
@@ -227,7 +227,7 @@ apply_estimator_to_fold("{config_path}", "{fold}")
 			log_file=path.join(CONFIG.project_path, fold, job_name + ".log"),
 			error_file=path.join(CONFIG.project_path, fold, job_name + ".errors"),
 			num_cores=CONFIG.n_jobs if CONFIG.n_jobs != -1 else 1, 
-			qsub_mail="-m a -M " + CONFIG.qsub_mail if qsub_mail != False else "",
+			qsub_mail="-m a -M " + CONFIG.qsub_mail if CONFIG.qsub_mail != False else "",
 			qsub_mem=CONFIG.qsub_mem
 			)
 		)
